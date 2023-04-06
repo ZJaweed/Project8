b3ae4a81ff7508723a8d2e276efaee7878761dde
jaweed <jaweed@LAPTOP-E0ODE6MM.mynetworksettings.com>
2023-04-06 16:03:26 -0400

    Initial Commit

---
+++ src/Test8/Test8.java
@@ -0,0 +1,13 @@
+package Test8;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Test8.....");
+		
+		 System.out.println(" project master");    
+	}
+
+}
